901e5239ecd7f217d1a7d9e08d778fb979b2ec4d
crashtack <crashtack@gmail.com>
2016-08-26 17:00:00 -0700

    populated database, hooked up "home" route"

--- learing_journal/models/mymodel.py
+++ learing_journal/models/mymodel.py
@@ -3,7 +3,9 @@ from sqlalchemy import (
     Index,
     Integer,
     Text,
-    UnicodeText
+    UnicodeText,
+    Unicode,
+    Date,
 )
 
 from .meta import Base
@@ -12,8 +14,8 @@ from .meta import Base
 class MyModel(Base):
     __tablename__ = 'models'
     id = Column(Integer, primary_key=True)
-    name = Column(UnicodeText)
-    value = Column(Integer)
+    title = Column(Unicode)
+    date = Column(Unicode)
+    body = Column(UnicodeText)
 
-
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+Index('my_index', MyModel.id, unique=True, mysql_length=255)
